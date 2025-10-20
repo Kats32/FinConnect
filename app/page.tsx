3418f19c1714a96b70842802b4a1cb14eab0adbf
Pranathi-Allu <pranathiallu2910@gmail.com>
2025-10-20 15:26:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 // app/page.tsx
+'use client';
 import Image from "next/image";
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
